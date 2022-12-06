5aa54c02e3364b67c2053e63289c159410dd8826
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-06 20:15:48 +0000

    Bomb placement complete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 let gridSquares;
 let padSize = 440;
 let gridSize = 16;
+let bombNumber = 10;
 
 
 
@@ -10,18 +11,22 @@ function newGame(){
     container.innerHTML = '';
     createSquares(gridSize);
     gridSquares = document.querySelectorAll('.square');
+    createBombs(bombNumber);
     gridSquares.forEach(element => element.addEventListener('click', squareClicked));
 }
 
 
 function squareClicked(){
-    console.log(this);
+
+    this.innerHTML = this.value;
+    console.log(this.value);
 }
 
 function createSquares(gridSize){
     let squares;
     let column;
     let row;
+    let id = 1;
     const border = 1;
     container.style.width = padSize + 'px'; 
     container.style.height = padSize + 'px'; 
@@ -34,10 +39,23 @@ function createSquares(gridSize){
             square.style.height = ((padSize / gridSize) - (2 * border)) + 'px';
             square.setAttribute('data-row', i);
             square.setAttribute('data-column', j);
+            square.setAttribute('id', id);
             container.appendChild(square);
-            console.log(`added square column ${i} row ${j}`);
+            console.log(`added square column ${i} row ${j} id ${id}`);
+            id++;
         }
     }
-   
+    return;
+}
+
+function createBombs(number){
+    console.log('generating bombs');
+    for (let i = 0; i < number; i++){
+        let columnIndex = Math.floor(Math.random() * gridSize) + 1; 
+        let rowIndex = Math.floor(Math.random() * gridSize) + 1;
+        let bombSquare = document.getElementById(((gridSize - 1) * columnIndex) + rowIndex); 
+        bombSquare.value = 'b';
+        bombSquare.style.backgroundColor = 'pink'
+    }
     return;
 }
