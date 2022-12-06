9ed1471aa4e1e205499b85bcf9138a9f84a88640
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-06 22:13:23 +0000

    Basic populkate numbers and select square added

--- script.js
+++ script.js
@@ -20,10 +20,78 @@ function newGame(){
 
 
 function squareClicked(){
-    this.innerHTML = this.value;
-    this.style.backgroundColor = 'grey';
+    getNumber(this.id);
     console.log(this);
     return;
+}
+
+function getNumber(id){
+    let clickedSquare = document.getElementById(id);
+    if (clickedSquare.value >= 1){
+        clickedSquare.innerHTML = clickedSquare.value;
+        clickedSquare.style.backgroundColor = 'grey';
+        return;
+    }
+    else if (clickedSquare.value == 'b'){
+        clickedSquare.innerHTML = clickedSquare.value;
+        clickedSquare.style.backgroundColor = 'black';
+        clickedSquare.style.color='red';
+        return;
+    }
+    /*
+    else {
+        let indexes;
+        console.log(`id: ${clickedSquare.id}`);
+        // if clicked square is top left
+        if (clickedSquare.id == 1){
+            indexes = [1 + 1, 1 + gridSize, 2 + gridSize];
+        }
+        // if clicked square is top right
+        else if (clickedSquare.id == gridSize){
+            indexes = [gridSize - 1, gridSize + gridSize, (gridSize - 1) + gridSize];
+        }
+        // if clicked square is bottom left
+        else if (clickedSquare.id == gridSize * (gridSize - 1) + 1){
+            let spot = gridSize * (gridSize - 1) + 1;
+            indexes = [spot + 1, spot - gridSize, spot - (gridSize + 1)];
+        } 
+        // if clicked square is bottom right
+        else if (clickedSquare.id == gridSize * gridSize){
+            let spot = gridSize * gridSize;
+            indexes = [spot - 1, spot - gridSize, spot - gridSize - 1];
+        }
+        // if clicked square is top row
+        else if (clickedSquare.getAttribute('data-row') == 1){
+            let spot = parseInt(clickedSquare.id);
+            indexes = [spot + 1, spot - 1, spot + gridSize - 1, spot + gridSize, spot + gridSize + 1];
+        }
+        // if clicked square is bottom row
+        else if (clickedSquare.getAttribute('data-row') == gridSize){
+            let spot = parseInt(clickedSquare.id);
+            indexes = [spot + 1, spot - 1, spot - gridSize + 1, spot - gridSize, spot - gridSize - 1];
+        }
+        // if clicked square is first column
+        else if (clickedSquare.getAttribute('data-column') == 1){
+            let spot = parseInt(clickedSquare.id);
+            indexes = [spot + 1, spot - gridSize, spot + gridSize, spot + gridSize + 1, spot - gridSize + 1 ];
+        }
+        // if clicked square is last column
+        else if (clickedSquare.getAttribute('data-column') == gridSize){
+            let spot = parseInt(clickedSquare.id);
+            indexes = [spot - 1, spot - gridSize, spot + gridSize, spot + gridSize - 1, spot - gridSize - 1 ];
+        }
+        // if square in middle 
+        else{
+            let spot = parseInt(clickedSquare.id);
+            console.log(spot);
+            indexes = [spot + 1, spot - 1, spot + gridSize, spot - gridSize, spot + gridSize - 1, spot + gridSize + 1, spot - gridSize - 1, spot - gridSize + 1];
+        }
+
+        for (let i = 0; i < indexes.length; i++){
+            console.log(`indexes: ${indexes}`);
+            getNumber(indexes[i]);
+        }
+    } */
 }
 
 function createSquares(gridSize){
@@ -77,170 +145,71 @@ function createNumbers(){
 }
 
 function populateNumbers(location){
-    
+    let indexCheck;
     // if bomb is in the top left corner
     if (location.id == 1){
-        let indexCheck = [1 + 1, 1 + gridSize, 2 + gridSize];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        indexCheck = [1 + 1, 1 + gridSize, 2 + gridSize];
     }
 
     // Bomb located in top right
     else if (location.id == gridSize){
-        let indexCheck = [gridSize - 1, gridSize + gridSize, (gridSize - 1) + gridSize];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        indexCheck = [gridSize - 1, gridSize + gridSize, (gridSize - 1) + gridSize];
     }
 
     // if bomb is bottom left
     else if (location.id == gridSize * (gridSize - 1) + 1){
         let spot = gridSize * (gridSize - 1) + 1;
-        let indexCheck = [spot + 1, spot - gridSize, spot - 15];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        indexCheck = [spot + 1, spot - gridSize, spot - gridSize + 1];
     }
     
     // If bomb is bottom right
     else if (location.id == gridSize * gridSize){
         let spot = gridSize * gridSize;
-        let indexCheck = [spot - 1, spot - gridSize, spot - 17];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        indexCheck = [spot - 1, spot - gridSize, spot - gridSize - 1];
     }
 
     // if bomb is in first row
     else if (location.getAttribute('data-row') == 1){
-        let spot = parseInt(location.id)
-        let indexCheck = [spot + 1, spot - 1, spot + 15, spot + 16, spot + 17];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        let spot = parseInt(location.id);
+        indexCheck = [spot + 1, spot - 1, spot + gridSize - 1, spot + gridSize, spot + gridSize + 1];
     }
 
     // if bomb is it the bottom row
     else if (location.getAttribute('data-row') == gridSize){
-        let spot = parseInt(location.id)
-        let indexCheck = [spot + 1, spot - 1, spot - gridSize + 1, spot - gridSize, spot - gridSize - 1];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        let spot = parseInt(location.id);
+        indexCheck = [spot + 1, spot - 1, spot - gridSize + 1, spot - gridSize, spot - gridSize - 1];
     }
 
     // if bomb in first column
     else if (location.getAttribute('data-column') == 1){
-        let spot = parseInt(location.id)
-        let indexCheck = [spot + 1, spot - gridSize, spot + gridSize, spot + gridSize + 1, spot - gridSize + 1 ];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        let spot = parseInt(location.id);
+         indexCheck = [spot + 1, spot - gridSize, spot + gridSize, spot + gridSize + 1, spot - gridSize + 1 ];
     }
 
     // if bomb in last column
     else if (location.getAttribute('data-column') == gridSize){
-        let spot = parseInt(location.id)
-        let indexCheck = [spot - 1, spot - gridSize, spot + gridSize, spot + gridSize - 1, spot - gridSize - 1 ];
-        for (let i = 0; i < indexCheck.length; i++){
-            console.log(document.getElementById(indexCheck[i]));
-            if (document.getElementById(indexCheck[i]).value == 'b'){
-                continue;
-            }
-            else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                document.getElementById(indexCheck[i]).value = 1;
-            }
-            else{
-                document.getElementById(indexCheck[i]).value++;
-            }
-        }
+        let spot = parseInt(location.id);
+        indexCheck = [spot - 1, spot - gridSize, spot + gridSize, spot + gridSize - 1, spot - gridSize - 1 ];
     }
 
         // bomb somewhere in center
-        else {
-            let spot = parseInt(location.id);
-            console.log(spot);
-            let indexCheck = [spot + 1, spot - 1, spot + gridSize, spot - gridSize, spot + gridSize - 1, spot + gridSize + 1, spot - gridSize - 1, spot - gridSize + 1];
-            console.log(indexCheck);
-            for (let i = 0; i < indexCheck.length; i++){
-                console.log(document.getElementById(i));
-                console.log(document.getElementById(indexCheck[i]));
-                if (document.getElementById(indexCheck[i]).value == 'b'){
-                    continue;
-                }
-                else if (isNaN(document.getElementById(indexCheck[i]).value)){
-                    document.getElementById(indexCheck[i]).value = 1;
-                }
-                else{
-                    document.getElementById(indexCheck[i]).value++;
-                }
-            }
+    else {
+        let spot = parseInt(location.id);
+        console.log(spot);
+        indexCheck = [spot + 1, spot - 1, spot + gridSize, spot - gridSize, spot + gridSize - 1, spot + gridSize + 1, spot - gridSize - 1, spot - gridSize + 1];
+        console.log(indexCheck);
+    }
+
+    for (let i = 0; i < indexCheck.length; i++){
+        if (document.getElementById(indexCheck[i]).value == 'b'){
+            continue;
         }
-        return;
-        
-    //}
+        else if (isNaN(document.getElementById(indexCheck[i]).value)){
+            document.getElementById(indexCheck[i]).value = 1;
+        }
+        else{
+            document.getElementById(indexCheck[i]).value++;
+        }
+    }
+    return;
 }
